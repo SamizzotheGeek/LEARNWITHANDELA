c7dd8abd2982934282dac7f8cff050cfdf7fcf06
SAMIZZO GEEKWORLD <40232208+SamizzotheGeek@users.noreply.github.com>
2019-06-22 06:24:41 -0700

    Add files via upload

--- cardGame/script.js
+++ cardGame/script.js
@@ -5,18 +5,38 @@ let suits = ['Hearts', 'Clubs', 'Diamonds','Spades'];
 let values = ['Ace', 'King', 'Queen ', 'Jack', 'Ten',
                 'Nine','Eight','Seven','Six','Five','Four','Three','Two'];
 
-//buttons
+//game incidences
+let gameStarted = false;
+    gameOver = false;
+    playerWon = false;
+    dealerCards = [];
+    dealerScore= 0;
+    dealScore = 0;
+    //deck = [];
+
+//DOM items
 let txt = document.getElementById('some');
 let newGame = document.getElementById('new');
 let hit = document.getElementById('hit');
 let pass = document.getElementById('pass');
 
 hit.style.display='none';
-pass.style = 'none';
+pass.style.display = 'none';
+showStatus();
+
+
 
 //new game
 newGame.addEventListener('click', function(){
-    txt.innerText = '<h2>Welcome to the BlackJack Card Game</h2>';
+    txt.innerText = 'Welcome to the BlackJack Card Game...';
+    hit.style.display= 'inline';
+    pass.style.display= 'inline';
+    newGame.style.display= 'none'
+
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+    showStatus();
 });
 
 //game dynamics
@@ -48,6 +68,25 @@ let deck=createDeck();
 }*/
 
 let deal=  [nextCard(),nextCard()];
+
+//status
+function showStatus(){ if (! gameStarted){
+    txt.innerText = 'Welcome to Blackjack card...enjoy';
+    return;
+}
+for (var i=0; i<deck.length; i++){
+    txt.innerText += '\n' + cardString(deck[i]);
+}
+}
+//shuffler
+function shuffler(deck){
+    for (let i=0; i<deck.length; i++){
+        let swapIdx = Math.trunc(Math.random()*deck.length);
+        let tmp = deck[swapIdx];
+        deck[swapIdx]=deck[i];
+        deck[i]=tmp;
+    }
+}
 
 console.log("Welcome to Poker");
 
